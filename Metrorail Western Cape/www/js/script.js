c740ee1a6cab4d8f45be868072737a14c9aa6c92
Einsteinium1458 <meyers.charl@gmail.com>
2016-04-20 12:05:52 +0200

    Made the context menu

---
+++ Metrorail Western Cape/www/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".navbar-toggle").click(function() {
+		//$("#homescreen").hide( "slide", { direction: "left"  }, 2000 );		
+		if($(this).data("slide") == true) {
+			$( "#body" ).animate({ left: "-=250px" }, "medium" );
+			$(this).data("slide", false);
+		} else {
+			$( "#body" ).animate({ left: "+=250px" }, "medium" );
+			$(this).data("slide", true);
+		}
+		//alert("click");
+	});
+});
